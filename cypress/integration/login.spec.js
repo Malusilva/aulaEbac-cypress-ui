c3d10b779d9aa4db02b8bb97d4169dfeec8a0ffc
Malusilva <maria.l.silva2386@gmail.com>
2022-06-08 12:20:58 -0300

    usando massa de dados, modulo 12

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
 context('Funcionalidade Login' , () =>{
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
  
     afterEach(() => {
@@ -18,7 +19,26 @@ it('Deve fazer login com sucesso' , () => {
  
         cy.get('.page-title').should('contain' , 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, aluno_ebac')
-}) 
+});
+
+it('Deve fazer login com sucesso - Usando arquivo  de dados', () => {
+    cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+ 
+        cy.get('.page-title').should('contain' , 'Minha conta') 
+
+});
+
+it.only('Deve fazer login com sucesso - Usando fixture', () => {
+    cy.fixture('perfil').then(dados => {
+        cy.get('#username').type(dados.usuario)
+        cy.get('#password').type(dados.senha, {log: false})
+        cy.get('.woocommerce-form > .button').click()
+ 
+        cy.get('.page-title').should('contain' , 'Minha conta') 
+    })
+});
 
 it('Deve exibir uma mensagem de erro ao inserir usuário inválido' , () =>{
     cy.get('#username').type('aluno_ebac@teste.com')
@@ -26,6 +46,6 @@ it('Deve exibir uma mensagem de erro ao inserir usuário inválido' , () =>{
         cy.get('.woocommerce-form > .button').click()
  
         cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail')
-})
+});
 
 })
--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -2,7 +2,7 @@
 
 describe('Funcionalidade Página de produtos', () => {
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        cy.visit('produtos')
     });
 
     it('Deve escolher um Produto da lista', () => {
@@ -10,22 +10,23 @@ describe('Funcionalidade Página de produtos', () => {
             //.first()
             //.last()
             //.eq(3)
-            .contains('Atlas Fitness Tank')
+            .contains('Abominable Hoodie')
             .click()
     });
 
     it.only('Deve adicionar produtos ao carrinho', () => {
-        var quantidade = 6
+        var quantidade = 2
 
         cy.get('[class="product-block grid"]')
-            .contains('Argus All-Weather Tank').click()
+            .contains('Abominable Hoodie').click()
+
         cy.get('.button-variable-item-L').click()
-        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+        cy.get('.button-variable-item-Red').click()
         cy.get('.input-text').clear().type(quantidade)
         cy.get('.single_add_to_cart_button').click()
 
         cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
-        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Argus All-Weather Tank” foram adicionados no seu carrinho.')
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
 
 
     });
