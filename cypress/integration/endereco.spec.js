c441219bd97a92cb87f0b78a448957a91998a0dc
Malusilva <maria.l.silva2386@gmail.com>
2022-06-27 21:43:53 -0300

    atualização dos dados de endereço

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -6,11 +8,33 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         cy.fixture('perfil').then(dados =>{
             cy.login(dados.usuario, dados.senha)
         })
+
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        //fazer login(Criar comando custumizado)
-        //fazer cadstro de endereço
+        EnderecoPage.editarEnderecoFaturamento('Maria', 'Luciano', 'EBAC', 'Brasil', 'Rua Nova', '2022', 'Rio de Janeiro', 'Rio de Janeiro', '01010101', '21232323232', 'qualquercoisa@gmail.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+
     });
+
+     //Ao trabalhar com lista o primeiro objeto é 0 o segundo é 1 e assim por diante 
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+
+    });
+
     
 });
